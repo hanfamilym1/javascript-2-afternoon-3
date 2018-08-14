c3dfa2bbff239837bfd72a14f237cb533a4a7e43
hanfamilym1 <michael@hanfamily.com>
2018-08-14 14:53:33 -0600

    Finished Product

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(arr, cb){
+  return cb(arr[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr, cb){
+  return cb(arr[arr.length-1])
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1,num2,cb){
+  return cb(num1*num2);
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +94,12 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains (arr, names, cb){
+  var i = 0;
+  if (arr[i] === names) {
+    return cb(true)
+  } return cb(false);
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +121,21 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(arr, cb){
+   
+  arr.sort();
+  
+ // possible for loop for that: stuck on duplicate values
+ for(let i =0; i < arr.length; i++){
+   
+   if (arr[i] === arr[i-1]){
+     arr.splice(i,1);
+     i--;
+   }
+ }
+ return cb(arr);
+}
+
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -124,6 +154,12 @@ uniq(names, function(uniqArr){
 
 //Code Here 
 
+function each( arr, cb){
+  for (let i = 0; i<arr.length; i++){
+   cb(arr[i], i);
+  }
+}
+
 // Do not edit the code below.
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
@@ -140,6 +176,13 @@ each(names, function(item, indice){
 */
 
 // Code here
+
+function getUserById(arr, id, cb){
+  for(let i = 0; i<arr.length; i++){
+    let user = arr[i]
+     return cb(user);
+    }
+  }
 
 // Do not edit the code below.
 var users = [
